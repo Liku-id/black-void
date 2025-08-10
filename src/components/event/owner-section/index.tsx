633b6a5962cd7cddda4fb06d0b0e069c16a5c797
Aldo Marcelino <aldo115marcelino@gmail.com>
2025-08-10 10:30:36 +0700

    fix promotor img

--- src/components/event/owner-section/index.tsx
+++ src/components/event/owner-section/index.tsx
@@ -21,13 +21,15 @@ const OwnerSection: React.FC<OwnerSectionProps> = ({
         type="heading"
         size={22}
         color="text-white"
-        className="leading-none">
+        className="leading-none"
+      >
         Terms & Conditions
       </Typography>
       <Typography
         type="body"
         size={12}
-        className="my-3 max-h-[120px] overflow-hidden text-white">
+        className="my-3 max-h-[120px] overflow-hidden text-white"
+      >
         {showFull
           ? termAndConditions
           : termAndConditions.slice(0, maxChar) + (isLong ? '...' : '')}
@@ -36,7 +38,8 @@ const OwnerSection: React.FC<OwnerSectionProps> = ({
         <button
           className="mt-1 text-xs text-blue-500 underline"
           onClick={() => setShowFull(!showFull)}
-          type="button">
+          type="button"
+        >
           {!showFull ? 'See details' : 'Show less'}
         </button>
       )}
@@ -45,26 +48,31 @@ const OwnerSection: React.FC<OwnerSectionProps> = ({
         type="heading"
         size={22}
         color="text-white"
-        className="mt-8 mb-3 lg:mt-10">
+        className="mt-8 mb-3 lg:mt-10"
+      >
         Promotor Info
       </Typography>
       <Box className="flex items-center gap-3">
-        {eventOrganizer?.asset?.url ? (
-          <Image
-            src={eventOrganizer?.asset?.url}
-            alt="Owner Logo"
-            width={48}
-            height={48}
-            className="mr-4 rounded-[14px]"
-          />
-        ) : (
-          <Box className="mr-4 h-12 w-12 rounded-[14px] bg-white" />
-        )}
+        <Box className="mr-4 flex h-12 w-12 items-center rounded-[14px] bg-white">
+          {eventOrganizer?.asset?.url ? (
+            <Image
+              src={eventOrganizer?.asset?.url}
+              alt="Owner Logo"
+              width={48}
+              height={48}
+              objectFit="contain"
+              unoptimized
+            />
+          ) : (
+            <Box className="mr-4 h-12 w-12 rounded-[14px] bg-white" />
+          )}
+        </Box>
         <Typography
           type="body"
           size={14}
           color="text-white"
-          className="font-bold">
+          className="font-bold"
+        >
           {eventOrganizer.name}
         </Typography>
       </Box>
