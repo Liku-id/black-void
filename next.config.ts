c6032cabaf6c4e7012508e489eab3123b8dc7fc1
edgbero <ebabero@gmail.com>
2026-01-30 17:57:11 +0700

    GEN-3449: sharp

--- next.config.ts
+++ next.config.ts
@@ -9,10 +9,25 @@ const nextConfig: NextConfig = {
 
   // Image optimization
   images: {
+    domains: [
+      'wukong-staging-public.s3.ap-southeast-3.amazonaws.com',
+      'wukong-staging-private.s3.ap-southeast-3.amazonaws.com',
+      'dummyimage.com',
+    ],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'dummyimage.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wukong-staging-public.s3.ap-southeast-3.amazonaws.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'wukong-staging-private.s3.ap-southeast-3.amazonaws.com',
         pathname: '/**',
       },
       {
