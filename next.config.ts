29c0a1310ce2c58d17570850c2ba2a21c0eb41ed
edbero <72855722+edbero@users.noreply.github.com>
2026-01-30 18:11:53 +0700

    [GEN-3449] Update Dockerfile (#226)
    * GEN-3449: performance improvement
    
    * GEN-3449: best practice
    
    * GEN-3449: image priority
    
    * GEN-3449: next config, cache
    
    * GEN-3449: fix blank image
    
    * GEN-3449: sharp
    
    * GEN-3449: fix docker
    
    ---------
    
    Co-authored-by: edgbero <ebabero@gmail.com>

--- next.config.ts
+++ next.config.ts
@@ -30,6 +30,11 @@ const nextConfig: NextConfig = {
         hostname: '*.s3.ap-southeast-3.amazonaws.com',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: '*.s3.ap-southeast-3.amazonaws.com',
+        pathname: '/**',
+      },
     ],
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
