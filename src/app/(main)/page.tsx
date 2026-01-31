0d7991e85452acc93b9b24d8980add633ddf6043
edbero <72855722+edbero@users.noreply.github.com>
2026-01-31 18:52:54 +0700

    cherry-pick 7a7b6a10fd53d9f173196290fb1219cb66a2d40a

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,6 +1,15 @@
 import dynamic from 'next/dynamic';
 import CarouselSection from '@/components/home/carousel-section';
 import { SEO_CONFIG } from '@/config/seo';
+import { getCarouselData } from '@/components/home/carousel-section/carousel.data';
+
+const EventListSection = dynamic(
+  () => import('@/components/home/event-list-section')
+);
+const CreatorListSection = dynamic(
+  () => import('@/components/home/creator-list-section')
+);
+const FAQSection = dynamic(() => import('@/components/home/faq-section'));
 
 const EventListSection = dynamic(
   () => import('@/components/home/event-list-section')
@@ -64,10 +73,12 @@ const faqs = [
 
 export const metadata = SEO_CONFIG.pages.home;
 
-export default function Home() {
+export default async function Home() {
+  const carouselItems = await getCarouselData();
+
   return (
     <main>
-      <CarouselSection />
+      <CarouselSection items={carouselItems} />
       <EventListSection />
       <CreatorListSection />
       <FAQSection data={faqs} />
