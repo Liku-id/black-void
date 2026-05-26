9ceb9438d42273dadfe8ee6621115e4343d187f1
edgbero <ebabero@gmail.com>
2026-05-26 11:57:20 +0700

    GEN-3795: add more translate

--- src/app/[locale]/(auth)/register/verify-otp/page.tsx
+++ src/app/[locale]/(auth)/register/verify-otp/page.tsx
@@ -1,16 +1,19 @@
 import { Box, Typography } from '@/components';
 import dynamic from 'next/dynamic';
+import { useTranslations } from 'next-intl';
 
 const VerifyOtpForm = dynamic(
   () => import('@/components/auth/verify-otp/verify-otp-form')
 );
 
 export default function VerifyOtpPage() {
+  const t = useTranslations('register.otp');
+
   return (
     <Box className="relative mx-auto mt-32 max-w-[370px] text-white xl:mr-0 xl:ml-30">
       <Box className="text-center">
         <Typography size={41} type="heading" className="font-normal uppercase">
-          {"let's get wu verified".split('').map((child, idx) => (
+          {t('title').split('').map((child, idx) => (
             <span className="hover-text" key={idx}>
               {child}
             </span>
